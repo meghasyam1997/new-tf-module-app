ec9a8f44c1951496593e6a22153eeb110509c1a9
meghasyam1997 <meghasyam01@gmail.com>
2023-09-19 16:21:52 +0530

    nat gateway

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_launch_template" "template" {
-  name                   = "${var.name}-${var.env}"
-  image_id               = data.aws_ami.ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_all_sg.id]
-}
 
 resource "aws_security_group" "allow_all_sg" {
   name        = "${var.name}-${var.env}"
@@ -38,23 +32,30 @@ resource "aws_security_group" "allow_all_sg" {
   }
 }
 
-resource "aws_autoscaling_group" "asg" {
-  name = "${var.name}-${var.env}-asg"
-  desired_capacity    = var.desired_capacity
-  max_size            = var.max_size
-  min_size            = var.min_size
-  vpc_zone_identifier = var.subnet_ids
 
-  launch_template {
-    id      = aws_launch_template.template.id
-    version = "$Latest"
-  }
-  dynamic "tag" {
-    for_each = local.asg_tags
-    content {
-      key                 = tag.key
-      propagate_at_launch = true
-      value               = tag.value
-    }
-  }
+resource "aws_launch_template" "template" {
+  name                   = "${var.name}-${var.env}-lt"
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_all_sg.id]
 }
+#resource "aws_autoscaling_group" "asg" {
+#  name                = "${var.name}-${var.env}-asg"
+#  desired_capacity    = var.desired_capacity
+#  max_size            = var.max_size
+#  min_size            = var.min_size
+#  vpc_zone_identifier = var.subnet_ids
+#
+#  launch_template {
+#    id      = aws_launch_template.template.id
+#    version = "$Latest"
+#  }
+#  dynamic "tag" {
+#    for_each = local.asg_tags
+#    content {
+#      key                 = tag.key
+#      propagate_at_launch = true
+#      value               = tag.value
+#    }
+#  }
+#}
