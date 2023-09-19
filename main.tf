7a7d908a325b477732c85997edeaf2b9f65c3a2f
meghasyam1997 <meghasyam01@gmail.com>
2023-09-19 16:24:03 +0530

    nat gateway

--- main.tf
+++ main.tf
@@ -39,23 +39,23 @@ resource "aws_launch_template" "template" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_all_sg.id]
 }
-#resource "aws_autoscaling_group" "asg" {
-#  name                = "${var.name}-${var.env}-asg"
-#  desired_capacity    = var.desired_capacity
-#  max_size            = var.max_size
-#  min_size            = var.min_size
-#  vpc_zone_identifier = var.subnet_ids
-#
-#  launch_template {
-#    id      = aws_launch_template.template.id
-#    version = "$Latest"
-#  }
-#  dynamic "tag" {
-#    for_each = local.asg_tags
-#    content {
-#      key                 = tag.key
-#      propagate_at_launch = true
-#      value               = tag.value
-#    }
-#  }
-#}
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.name}-${var.env}-asg"
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+  dynamic "tag" {
+    for_each = local.asg_tags
+    content {
+      key                 = tag.key
+      propagate_at_launch = true
+      value               = tag.value
+    }
+  }
+}
