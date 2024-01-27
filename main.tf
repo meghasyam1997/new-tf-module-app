7a8d28ccfb142df92b29cf50352d5e4a71402a2a
meghasyam1997 <meghasyam01@gmail.com>
2024-01-27 16:59:11 +0530

    instance

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   subnet_id     = var.subnet_ids
+
+  tags = {
+    Name = "${var.name}-${var.env}"
+  }
 }
