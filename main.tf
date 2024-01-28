5bd6aee122604ba68c6bbe3cd4287159ff2d6088
meghasyam1997 <meghasyam01@gmail.com>
2024-01-28 13:23:35 +0530

    autoscaling group

--- main.tf
+++ main.tf
@@ -1,9 +1,49 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_ids
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-${var.env}"
+  description = "${var.name}-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "APP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.allow_app_cidr
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "${var.name}-${var.env}"
+  }
+}
+resource "aws_launch_template" "main" {
+  name_prefix            = "${var.name}-${var.env}"
+  image_id               = data.aws_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+  min_size         = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
   }
 }
--- vars.tf
+++ vars.tf
@@ -1,4 +1,13 @@
 variable "instance_type" {}
 variable "name" {}
-variable "env" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+
+variable "bastion_cidr" {}
+variable "allow_app_cidr" {}
+variable "vpc_id" {}
 variable "subnet_ids" {}
+
+variable "tags" {}
+variable "env" {}
